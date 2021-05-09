d1009e155cc2493c8aa8d38945b3b412cbfa562d
Darek Kay <hello@darekkay.com>
2021-05-09 17:56:14 +0200

    Enable class fields proposal plugin in Babel

--- src/config/jest/babelrc.js
+++ src/config/jest/babelrc.js
@@ -4,10 +4,28 @@ const { ifAnyDep, ifTypescript } = require("../../utils");
  * Babel configuration for Jest tests.
  */
 module.exports = () => ({
+  assumptions: {
+    // When using public class fields, assume that they don't shadow any getter in the current class, in its subclasses or in its superclass. Thus, it's safe to assign them rather than using Object.defineProperty.
+    setPublicClassFields: true,
+  },
+
   presets: [
     [require.resolve("@babel/preset-env")],
-    ifAnyDep("react", [require.resolve("@babel/preset-react")]),
+    ifAnyDep("react", [
+      require.resolve("@babel/preset-react"),
+      {
+        // Adds component stack to warning messages
+        // Adds __self attribute to JSX which React will use for some warnings
+        development: true,
+      },
+    ]),
     ifAnyDep("vite", [require.resolve("babel-preset-vite")]),
     ifTypescript([require.resolve("@babel/preset-typescript")]),
   ].filter(Boolean),
+
+  plugins: [
+    // Enable class fields proposal
+    // https://github.com/tc39/proposal-class-fields
+    [require.resolve("@babel/plugin-proposal-class-properties")],
+  ],
 });
