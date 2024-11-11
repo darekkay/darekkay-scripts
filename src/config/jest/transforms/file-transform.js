0313b25ad1e39e834843ffc3009691ad0e47c7c3
Darek Kay <hello@darekkay.com>
2024-11-11 17:59:57 +0100

    Update dependencies

--- src/config/jest/transforms/file-transform.js
+++ src/config/jest/transforms/file-transform.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require("node:path");
 
 /**
  * A custom Jest transformer turning file imports into file names.
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const logger = require("@darekkay/logger");
 const arrify = require("arrify");
